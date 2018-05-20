d4cc3db87338781398e37f0dbde4934c0bd24256
Wikan Wicaksono <komowicakomo@gmail.com>
2018-05-20 09:40:30 +0700

    updates sticky element and buttons

--- app/js/view/detail_atraksi.js
+++ app/js/view/detail_atraksi.js
@@ -1,6 +1,20 @@
 
 
  $(document).ready(function () {
+  var parents = $('.detail-container')
+  var pos = $('.op-detail__price').offset().left
+
+  console.log(pos)
+
+  $(".op-detail__price").stick_in_parent({
+    parent : parents,
+    offset_top : 80
+  }).on("sticky_kit:stick", function(e) {
+    e.target.style.left = pos + 'px'
+    console.log(e.target.style)
+  }).on("sticky_kit:unstick", function(e) {
+    e.target.style.left = 0 + 'px'
+  });
 
    var galleryTop = new Swiper('.gallery-top', {
        effect : 'fade'
