b6ba4b66272fe4e8abb3d1daf1ffbfc65b0ba94e
Wikan Wicaksono <komowicakomo@gmail.com>
2018-07-10 22:38:46 +0700

    sticky fix , gallery added to opentrip detail , maps added to opentrip detail

--- app/js/view/detail_opentrip.js
+++ app/js/view/detail_opentrip.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-  var parents = $('.detail-container')
+  var parents = $('#detail--containers')
   var pos = $('.op-detail__price').offset().left
 
   $(".op-detail__price").stick_in_parent({
@@ -8,6 +8,7 @@ $(document).ready(function(){
     offset_top : 80
   }).on("sticky_kit:stick", function(e) {
     e.target.style.left = pos + 'px'
+    e.target.style.top = 80 + 'px'
     console.log(e.target.style)
   }).on("sticky_kit:unstick", function(e) {
     e.target.style.left = 0 + 'px'
